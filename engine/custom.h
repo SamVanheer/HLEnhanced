7aaf98aa5e117eb93c75608de134cb999866de01
Sam <duo929@hotmail.com>
2017-11-03 21:01:57 +0100

    Convert resource flags defines into enum

--- engine/custom.h
+++ engine/custom.h
@@ -50,14 +50,16 @@ struct resourceinfo_t
 	_resourceinfo_t info[ 8 ];
 };
 
-#define RES_FATALIFMISSING (1<<0)   // Disconnect if we can't get this file.
-#define RES_WASMISSING     (1<<1)   // Do we have the file locally, did we get it ok?
-#define RES_CUSTOM         (1<<2)   // Is this resource one that corresponds to another player's customization
-								    // or is it a server startup resource.
-#define RES_REQUESTED	   (1<<3)	// Already requested a download of this one
-#define RES_PRECACHED	   (1<<4)	// Already precached
-#define RES_ALWAYS		   (1<<5)	// download always even if available on client	
-#define RES_CHECKFILE	   (1<<7)	// check file on client
+enum ResFlag
+{
+	RES_FATALIFMISSING	=	1	<<	0,	//! Disconnect if we can't get this file.
+	RES_WASMISSING		=	1	<<	1,	//! Do we have the file locally, did we get it ok?
+	RES_CUSTOM			=	1	<<	2,	//! Is this resource one that corresponds to another player's customization or is it a server startup resource.
+	RES_REQUESTED		=	1	<<	3,	//! Already requested a download of this one
+	RES_PRECACHED		=	1	<<	4,	//! Already precached
+	RES_ALWAYS			=	1	<<	5,	//! download always even if available on client	
+	RES_CHECKFILE		=	1	<<	7,	//! check file on client
+};
 
 #include "crc.h"
 
